dec4d085913f08c6067b6bc894eb909020ea9b62
Eoin Kelly <eoin.kelly@ackama.com>
2024-08-16 08:20:56 +1200

    Initial commit

---
+++ src/entrypoint.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+set -e
+
+if [ -z "$1" ] # nothing specified so we bootstrap the service itself
+then
+    manage migrate --noinput
+    exec gunicorn -c gunicorn.conf.py turbo_demo.main.asgi:application
+elif [ "$1" = "run" ]  # run the service only
+then
+    exec gunicorn -c gunicorn.conf.py turbo_demo.main.asgi:application
+elif [ "$1" = "migrate" ]  # run database migrations
+then
+    exec manage migrate --noinput "${@:2}"
+elif [ "$1" = "bash" ]  # run a regular bash shell
+then
+    exec bash "${@:2}"
+else # pass any other params directly to manage.py to handle
+    exec manage "$@"
+fi
---
+++ src/turbo_demo/main/settings.py
@@ -0,0 +1,146 @@
+from pathlib import Path
+
+import environ
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+
+# Take environment variables from .env file
+environ.Env.read_env(env_file=BASE_DIR.parent.parent / ".env")
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+SECRET_KEY: str = env("SECRET_KEY")
+DEBUG: bool = env.bool("DEBUG", default=False)
+ALLOWED_HOSTS: list[str] = env.list("ALLOWED_HOSTS", default=[])
+GIT_COMMIT_HASH: str = env("GIT_COMMIT_HASH")
+GIT_COMMIT_TIME: str = env.int("GIT_COMMIT_TIME")
+GIT_COMMIT_COUNT: int = env.int("GIT_COMMIT_COUNT")
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    # Built in
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # Third party
+    # Local
+    "turbo_demo.accounts",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "turbo_demo.main.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "turbo_demo.main.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    # read os.environ['DATABASE_URL'] and raises
+    # ImproperlyConfigured exception if not found
+    #
+    # The db() method is an alias for db_url().
+    "default": env.db(default="postgres://dev:dev_password@localhost:5432/dev"),
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: E501
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+AUTH_USER_MODEL = "accounts.User"
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = "en-au"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Logging
+LOG_LEVEL = env("APP_LOG_LEVEL", default="INFO")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": LOG_LEVEL,
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": LOG_LEVEL,
+            "propagate": True,
+        },
+    },
+}
